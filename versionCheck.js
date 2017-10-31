7a7fa215bca24165393435e78ea6f0b2f2535d2a
Aaron Cordova <theqodesmith@gmail.com>
2017-10-31 09:48:07 -0400

    Please let the preinstall stuff be resolved -_-

--- versionCheck.js
+++ versionCheck.js
@@ -2,12 +2,14 @@
 // Possible permissions errors? - https://goo.gl/mH9n3j
 
 const nodeVersion = process.versions.node;
+const spaces = ' '.repeat(43 - nodeVersion.length);
 
 if (nodeVersion[0] < 6) {
   const message = `
     +-------------------------------------------------------------------+
     |                                                                   |
     |  Create New App requires Node >= 6 and npm >= 3.                  |
+    |  You're using version ${nodeVersion}.${spaces}|
     |  Please upgrade. The easiest way is to use Node Version Manager:  |
     |    https://github.com/creationix/nvm                              |
     |                                                                   |

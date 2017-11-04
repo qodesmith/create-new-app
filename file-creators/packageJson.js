780e7829047ab699af2f60d3dc9d7bafa3df4105
Aaron Cordova <theqodesmith@gmail.com>
2017-11-04 06:15:59 -0400

    Refactored packageJson function.

--- file-creators/packageJson.js
+++ file-creators/packageJson.js
@@ -1,52 +1,37 @@
 function packageJson(answers) {
   const { appName, server, description, author, email, keywords } = answers;
-  const phRemove = 'PLACEHOLDER-REMOVE';
-  const phKeywords = 'PLACEHOLDER-KEYWORDS';
-  const json = `
-    {
-      "name": "${appName}",
-      "version": "0.1.0",
-      "description": "${description}",
-      ${server ? `"main": "server.js",` : phRemove}
-      "scripts": {
-        "build": "cross-env NODE_ENV=production webpack",
-        ${server ? `"build:dev": "cross-env NODE_ENV=development webpack",` : phRemove}
-        ${server ? `"local": "npm run server:api",` : phRemove}
-        ${server ? `"server:dev": "webpack-dev-server --open",` : phRemove}
-        ${server ? `"server:api": "nodemon server.js",` : phRemove}
-        ${server ? `"start": "cross-env NODE_ENV=development npm-run-all --parallel server:*"` : phRemove}
-        ${!server ? `"start": "cross-env NODE_ENV=development webpack-dev-server --open"` : phRemove}
-      },
-      "keywords": ${phKeywords},
-      "author": "${author}",
-      "email": "${email}",
-      "license": "MIT",
-      "browserslist": "last 2 versions"
-    }
-  `.split('\n').slice(1);
+  const json = [
+    '{',
+    `  "name": "${appName}",`,
+    `  "version": "0.1.0",`,
+    `  "description": "${description}",`,
+    server && `  "main": "server.js",`,
+    `  "scripts": {`,
+    `    "build": "cross-env NODE_ENV=production webpack"`,
+    server && `    "build:dev": "cross-env NODE_ENV=development webpack",`,
+    server && `    "local": "npm run server:api",`,
+    server && `    "server:dev": "webpack-dev-server --open",`,
+    server && `    "server:api": "nodemon server.js",`,
+    server && `    "start": "cross-env NODE_ENV=development npm-run-all --parallel server:*"`,
+    !server && `    "start": "cross-env NODE_ENV=development webpack-dev-server --open"`,
+    `  },`,
+    `  "keywords": [PLACEHOLDER-KEYWORDS],`,
+    `  "author": "${author}",`,
+    `  "email": "${email}",`,
+    `  "license": "MIT",`,
+    `  "browserslist": "last 2 versions"`,
+    '}',
+    ''
+  ];
 
-  // Do a bunch of leading-space calculation stuffs.
-  // We want the final `package.json` file to look good.
-  const indent = json[0].search(/\S/); // https://goo.gl/DirJ71
-  const kwIndex = json.findIndex(line => line.includes(phKeywords));
-  const kwIndent = json[kwIndex].search(/\S/) + 2;
-  const kwSpaces = ' '.repeat(kwIndent);
-  const lastSpaces = ' '.repeat(kwIndent - 2);
-  const lastIndex = keywords.length - 1;
   const kwds = keywords.reduce((acc, kw, i) => {
-    return `${acc}\n${kwSpaces}"${kw}"${i === lastIndex ? '' : ','}`;
+    return `${acc}\n    "${kw}"${i === keywords.length - 1 ? '\n  ' : ','}`;
   }, '');
-  const kwdsArr = keywords.length ? `[${kwds}\n${lastSpaces}]` : '[]';
-
-  // Resolve the keywords array.
-  json[kwIndex] = json[kwIndex].replace(phKeywords, kwdsArr);
 
   return json
-    .filter(line => line.trim() !== phRemove)
-    .join('\n')  //  \  These two lines are needed to avoid weird issues
-    .split('\n') //  /  with the newline characters in `kwds`.
-    .map(line => line.slice(indent))
-    .join('\n');
+    .filter(Boolean)
+    .join('\n')
+    .replace('PLACEHOLDER-KEYWORDS', kwds);
 }
 
 module.exports = packageJson;

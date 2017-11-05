662e73786ae8d778b77d08a1918fa4410d32e9fb
Aaron Cordova <theqodesmith@gmail.com>
2017-11-05 08:55:45 -0500

    Exporting fxn.

--- modules/showVersion.js
+++ modules/showVersion.js
@@ -1,2 +1,6 @@
-const json = require('../package.json');
-console.log(json.version);
+function showVersion() {
+  const json = require('../package.json');
+  console.log(json.version);
+}
+
+module.exports = showVersion;

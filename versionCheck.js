d782c87bb6e53c5206e83d9c5cee32fe0ee3afad
Aaron Cordova <theqodesmith@gmail.com>
2018-02-12 18:06:57 -0500

    Added styling to node version message.

--- versionCheck.js
+++ versionCheck.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 // Possible permissions errors? - https://goo.gl/mH9n3j
 
+const chalk = require('chalk');
 const nodeVersion = process.versions.node;
 const spaces = ' '.repeat(43 - nodeVersion.length);
+const url = chalk.cyan.bold('https://github.com/creationix/nvm');
 
 if (nodeVersion[0] < 6) {
   const message = `
@@ -11,7 +13,7 @@ if (nodeVersion[0] < 6) {
     |  Create New App requires Node >= 6 and npm >= 3.                  |
     |  You're using version ${nodeVersion}.${spaces}|
     |  Please upgrade. The easiest way is to use Node Version Manager:  |
-    |    https://github.com/creationix/nvm                              |
+    |    ${url}                              |
     |                                                                   |
     +-------------------------------------------------------------------+
   `;

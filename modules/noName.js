3d28f9237f829c7fdc0ff1a94fd9eb25c624376a
Aaron Cordova <theqodesmith@gmail.com>
2018-02-12 19:05:27 -0500

    Added message to use guided process.

--- modules/noName.js
+++ modules/noName.js
@@ -6,7 +6,9 @@ function noName() {
   console.log('Please specify the project directory:');
   console.log(`  ${cna} ${chalk.green('<project-directory>')}\n`);
   console.log('For example:');
-  console.log(`  ${cna} ${chalk.green('my-awesome-project')}`);
+  console.log(`  ${cna} ${chalk.green('my-awesome-project')}\n`);
+  console.log('Or you can use the guided process:');
+  console.log(`  ${cna}`);
 }
 
 module.exports = noName;

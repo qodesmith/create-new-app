8cbf135c26ee133673454c207e8b9cc05885e1e3
Aaron Cordova <theqodesmith@gmail.com>
2018-02-13 17:11:10 -0500

    Adjusted `titleCase` fxn to account for underscores.

--- modules/titleCase.js
+++ modules/titleCase.js
@@ -1,6 +1,7 @@
 function titleCase(txt) {
   if (typeof txt !== 'string' || !txt.length) return '';
   return txt
+    .replace(/_/g, '-')
     .split('-')
     .map(word => word[0].toUpperCase() + word.slice(1))
     .join(' ');

b6a8495549cee44e6662135bba84d11431ad4930
Aaron Cordova <theqodesmith@gmail.com>
2018-02-13 17:14:22 -0500

    Added utility fxn to check if directory exists.

---
+++ modules/checkDirExists.js
@@ -0,0 +1,13 @@
+// Check if the directory already exists.
+const { existsSync } = require('fs-extra');
+const chalk = reuire('chalk');
+
+function checkDirExists({ appDir, appName, force }) {
+  if (!existsSync(appDir)) return;
+  console.log(`The directory ${chalk.green(appName)} already exists.`);
+
+  if (force) return console.log('Force installing in pre-existing directory...');
+  console.log('Try a different name.') && process.exit();
+}
+
+module.exports = checkDirExists;

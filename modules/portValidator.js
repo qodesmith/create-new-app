149b0bb057ecbf9f15b2f56964e845754ef2577a
Aaron Cordova <theqodesmith@gmail.com>
2018-04-06 19:12:03 -0400

    Updated the port validator logic.

--- modules/portValidator.js
+++ modules/portValidator.js
@@ -1,17 +1,18 @@
 const chalk = require('chalk');
 
 function portValidator(val, type) {
-  const num = Number.isInteger(+val) && parseInt(val, 10);
-  const defaultPort = type === 'api' ? 3000 : 8080;
+  const num = Number.isInteger(+val) ? (+val) : undefined;
+  const defaultPorts = { api: 3000, dev: 8080 }
+  const defaultPort = defaultPorts[type];
 
-  if (num < 1 || num > 65535) {
-    const msg = `\n"${num}" is out of range (1 - 65535). Defaulting to ${defaultPort}...\n`;
+  if (num === undefined) {
+    const msg = `\n"${val}" is an invalid port. Defaulting to ${defaultPort}...\n`;
     console.log(chalk.yellow.bold(msg));
     return defaultPort;
   }
 
-  if (!num) {
-    const msg = `\n"${val}" is an invalid port. Defaulting to ${defaultPort}...\n`;
+  if (num < 1 || num > 65535) {
+    const msg = `\n"${num}" is out of range (1 - 65535). Defaulting to ${defaultPort}...\n`;
     console.log(chalk.yellow.bold(msg));
     return defaultPort;
   }

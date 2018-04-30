ecd5aaa7a3d86518c006555b0084f4f8c7af09a3
Aaron Cordova <theqodesmith@gmail.com>
2018-04-29 20:39:12 -0400

    Refactored sample utility helper fxn.

--- files/redux/utils/helpers/index.js
+++ files/redux/utils/helpers/index.js
@@ -1,12 +1,12 @@
 export const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
-const letters = ['a' ,'b' ,'c' ,'d' ,'e' ,'f']
-const numbers = [...Array(10).keys()]
+const letters = 'abcdef'
+const numbers = '0123456789'
 export const randomHexColor = (hex = '') => {
   if (hex.length === 6) return `#${hex}`
 
-  const arr = randomNum(0,1) ? letters : numbers
-  const num = randomNum(0, arr.length - 1)
+  const set = randomNum(0,1) ? letters : numbers
+  const num = randomNum(0, set.length - 1)
 
-  return randomHexColor(`${hex}${arr[num]}`)
+  return randomHexColor(`${hex}${set[num]}`)
 }

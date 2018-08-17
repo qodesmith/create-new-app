0556e683bb66c026dab3cca8e8108177b58b3c82
Aaron Cordova <theqodesmith@gmail.com>
2018-08-16 20:19:21 -0400

    Refactored the randomHexColor fxn.

--- files/redux/utils/helpers/index.js
+++ files/redux/utils/helpers/index.js
@@ -5,11 +5,10 @@ export const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1
 
 const letters = 'abcdef'
 const numbers = '0123456789'
-export const randomHexColor = (hex = '') => {
-  if (hex.length === 6) return `#${hex}`
-
-  const set = randomNum(0,1) ? letters : numbers
-  const num = randomNum(0, set.length - 1)
-
-  return randomHexColor(`${hex}${set[num]}`)
-}
+export const randomHexColor = () => (
+  letters.split('').reduce(hex => {
+    const set = randomNum(0, 1) ? letters : numbers
+    const num = randomNum(0, set.length - 1)
+    return `${hex}${set[num]}`
+  }, '#')
+)

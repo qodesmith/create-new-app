4e18a36b939414c0b48a1eb427771c96ebbda4e7
Aaron Cordova <theqodesmith@gmail.com>
2018-11-10 08:47:54 -0500

    Fancy table logging with colors!

--- checkDependencies.js
+++ checkDependencies.js
@@ -1,13 +1,15 @@
+const chalk = require('chalk')
 const readline = require('readline')
 const run = require('./modules/run')
 const formDependencies = require('./modules/dependencies')
 const { devDependencies, serverDependencies } = formDependencies(true, true, true)
 const fullList = { ...devDependencies, ...serverDependencies }
-
-console.log('Generating table...\n')
 const table = []
-Object.keys(fullList).forEach((name, i, arr) => {
-  logInfo(i, arr)
+const keys = Object.keys(fullList)
+const keysLength = keys.length
+
+keys.forEach((name, i) => {
+  logInfo({ name, i })
 
   const latest = run(`npm view ${name} version`, true).toString('utf-8').split('\n')[0]
   const usedVersion = fullList[name]
@@ -16,11 +18,26 @@ Object.keys(fullList).forEach((name, i, arr) => {
   if (used[0] !== latest[0]) table.push({ package: name, used, latest })
 })
 
-if (!table.length) console.log('All packages up to date!')
+if (!table.length) {
+  clearConsole()
+  console.log('All packages up to date!')
+} else {
+  logInfo({ end: true })
+}
 
-function logInfo(i, arr) {
+function logInfo({ name, i, end }) {
+  clearConsole()
+
+  const num = chalk.yellow(i + 1)
+  const length = chalk.green(keysLength)
+  const pkg = chalk.blue(name)
+
+  name && console.log(`${num} of ${length} - checking ${pkg}`)
+  end && console.log(`Total of ${length} packages checked!`)
+  if (table.length) console.table(table)
+}
+
+function clearConsole() {
   readline.cursorTo(process.stdout, 0, 0)
   readline.clearScreenDown(process.stdout)
-  console.log(`${i + 1} of ${arr.length}`)
-  if (table.length) console.table(table)
 }

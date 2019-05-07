0a34798faceffc6dd470338a7537442b48d43cfe
Aaron Cordova <theqodesmith@gmail.com>
2019-05-07 17:10:35 -0400

    Updated checkDependencies.js

--- checkDependencies.js
+++ checkDependencies.js
@@ -1,30 +1,23 @@
 const chalk = require('chalk')
 const formDependencies = require('./modules/dependencies')
 const makeTable = require('./modules/makeTable')
+const removeAnsiChars = require('./modules/removeAnsiChars')
 const { devDependencies, serverDependencies } = formDependencies({ mongo: 1, redux: 1, router: 1, server: 1 })
 const fullList = { ...devDependencies, ...serverDependencies }
 const packages = Object.keys(fullList)
 const keysLength = packages.length
 const all = process.argv.some(arg => arg === '--all')
-const table = [[
-  chalk.bold('PACKAGE'),
-  chalk.bold('USED'),
-  chalk.bold('LATEST')
-]]
 
 const RegistryClient = require('npm-registry-client')
 const client = new RegistryClient()
 const uri = 'https://registry.npmjs.org'
 const params = { timeout: 1000 }
 
-const promises = packages.map((name, i) => {
+const promises = packages.map(name => {
   return new Promise(resolve => {
     const url = `${uri}/${name}/`
     client.get(url, params, (error, data, raw, res) => {
-      if (error) {
-        table.push([`${chalk.yellow.bold('ERROR')} - ${name}`, '-', '-'])
-        return resolve(null)
-      }
+      if (error) return resolve({ name: `${chalk.yellow.bold('ERROR')} - ${name}`, error: true })
 
       const usedVersion = fullList[name]
       const latestVersion = data['dist-tags'].latest
@@ -34,29 +27,42 @@ const promises = packages.map((name, i) => {
       name = isDeprecated ? `${chalk.bold.red('DEPRECATED:')} ${name}` : name
 
       if (all || isDeprecated || used !== latestMajor) {
-        table.push([name, used, chalk.green(latestVersion)])
+        resolve({ name, used, latestVersion: chalk.green(latestVersion) })
+      } else {
+        resolve({ name: '' })
       }
-
-      resolve(true)
     })
   })
 })
 
 Promise.all(promises).then(results => {
+  const finalResults = results
+    .sort((a, b) => {
+      const name1 = removeAnsiChars(a.name)
+      const name2 = removeAnsiChars(b.name)
+      return name1 > name2 ? 1 : name1 < name2 ? -1 : 0
+    })
+    .reduce((acc, { name, used, latestVersion, error }) => {
+      if (!name) return acc
+      if (error) return [...acc, [name, '-', '-']]
+
+      return [...acc, [name, used, latestVersion]]
+    }, [[chalk.bold('PACKAGE'), chalk.bold('USED'), chalk.bold('LATEST')]])
+
   // https://gist.github.com/KenanSulayman/4990953
   console.log('\033c') // Clear the console. I believe this is the `ESC` ASCII character.
 
   // Using a quick `setTimeout` here to ensure the console was cleared.
   setTimeout(() => {
-    const totalChecked = results.filter(Boolean).length
+    const totalChecked = results.filter(item => !item.error).length
     const checked = chalk[totalChecked === keysLength ? 'green' : 'yellow'](totalChecked)
 
-    if (!table.length) {
+    if (finalResults.length === 1) {
       console.log('All packages up to date!')
     } else {
       const total = chalk.green(keysLength)
       console.log(`${total} total packages, ${checked} checked!`)
-      console.log(makeTable(table, { centered: true }))
+      console.log(makeTable(finalResults, { centered: true }))
     }
   }, 100)
 })

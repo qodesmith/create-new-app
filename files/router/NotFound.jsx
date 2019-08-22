061b75ec357514dda7f5e59079652e40eaf5d749
Aaron Cordova <qodesmith@fb.com>
2019-08-22 06:16:24 -0400

    Components using function block. Much easier for users to update and tweak.

--- files/router/NotFound.jsx
+++ files/router/NotFound.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const NotFound = () => (
-  <>
-    <h1>Looking for something?</h1>
-    <p>
-      <span className="i">{window.location.href}</span> doesn't exist.
-    </p>
-  </>
-)
+const NotFound = () => {
+  return (
+    <>
+      <h1>Looking for something?</h1>
+      <p>
+        <span className="i">{window.location.href}</span> doesn't exist.
+      </p>
+    </>
+  )
+}
 
 export default NotFound

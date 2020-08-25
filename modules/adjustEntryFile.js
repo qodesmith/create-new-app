c3f186524302e4c1b2615add4626d46f7d40dfa3
Qodesmith <theqodesmith@gmail.com>
2020-08-25 08:35:13 -0400

    Fixed adjustEntryFile module test.

--- modules/adjustEntryFile.js
+++ modules/adjustEntryFile.js
@@ -3,17 +3,21 @@
   and adds a top-level comment to it explaining polyfill options.
 */
 
-function adjustEntryFile(entryFileContents) {
-  const commentContents = [
-    '/*',
-    '  http://bit.ly/2DTXGpe - `@babel/polyfill` has been deprecated.',
-    '  If you need to polyfill certain JS features, uncomment the two imports below.',
-    '*/',
-    `// import 'core-js/stable'`,
-    `// import 'regenerator-runtime/runtime' // Needed to polyfill async / await.`,
-  ].join('\n')
 
+const commentContents = [
+  '/*',
+  '  http://bit.ly/2DTXGpe - `@babel/polyfill` has been deprecated.',
+  '  If you need to polyfill certain JS features, uncomment the two imports below.',
+  '*/',
+  `// import 'core-js/stable'`,
+  `// import 'regenerator-runtime/runtime' // Needed to polyfill async / await.`,
+].join('\n')
+
+function adjustEntryFile(entryFileContents) {
   return entryFileContents.replace('__PLACEHOLDER_ENTRY_COMMENT__', commentContents)
 }
 
-module.exports = adjustEntryFile
+module.exports = {
+  adjustEntryFile,
+  commentContents,
+}
--- tests/unit/adjustEntryFile.test.js
+++ tests/unit/adjustEntryFile.test.js
@@ -1,20 +1,12 @@
-const adjustEntryFile = require('../../modules/adjustEntryFile')
+const { adjustEntryFile, commentContents } = require('../../modules/adjustEntryFile')
 const providedContent = 'abc'
 const results = adjustEntryFile(`__PLACEHOLDER_ENTRY_COMMENT__\n${providedContent}`)
 const lines = results.split('\n')
-const commentContents = [
-  '/*',
-  '  http://bit.ly/2DTXGpe - `@babel/polyfill` has been deprecated.',
-  '  If you need to polyfill certain JS features, uncomment the two imports below.',
-  '  Be sure to adjust the `browserslist` field in your `package.json` file.',
-  '*/',
-  "// import 'core-js/stable'",
-  "// import 'regenerator-runtime/runtime' // Needed to polyfill async / await."
-]
+
 
 describe('adjustEntryFile', () => {
   it('should generated a multi-line comment at the top of the file', () => {
-    commentContents.forEach((line, i) => expect(lines[i]).toBe(line))
+    commentContents.split('\n').forEach((line, i) => expect(lines[i]).toBe(line))
   })
 
   it('should indent the multi-line comment from 0.', () => {
@@ -22,7 +14,7 @@ describe('adjustEntryFile', () => {
   })
 
   it('should write the provided contents to the file', () => {
-    const expectedResults = [...commentContents, providedContent].join('\n')
+    const expectedResults = [...commentContents.split(), providedContent].join('\n')
     expect(results).toBe(expectedResults)
   })
 })

51f8019069dd74029bb2c287b4df50bd8901a052
Qodesmith <theqodesmith@gmail.com>
2020-12-20 09:53:08 -0500

    Updated Webpack and Sassyons deps.

--- modules/dependencies.js
+++ modules/dependencies.js
@@ -22,7 +22,7 @@ const dependencies = ({ mongo, redux, router, server }) => {
     // MAIN
     react: '^17',
     'react-dom': '^17',
-    sassyons: '^4',
+    sassyons: 'latest', // Always install the latest.
     redux: redux && '^4',
     'react-redux': redux && '^7',
     'react-router-dom': router && '^5',
@@ -34,24 +34,23 @@ const dependencies = ({ mongo, redux, router, server }) => {
     postcss: '^7',
     cssnano: '^4',
     '@fullhuman/postcss-purgecss': '^2', // http://bit.ly/2Xtfwao - why we're using purge-css *here* and not as a Webpack plugin.
-    'purgecss-whitelister': 'latest', // Always install latest.
+    'purgecss-whitelister': 'latest', // Always install the latest.
     'postcss-combine-media-query': '^1',
     'postcss-combine-duplicated-selectors': '^8',
     autoprefixer: '^9',
     'css-declaration-sorter': '^5',
 
     // WEBPACK
-    webpack: '^4',
-    'webpack-cli': '^3',
-    'webpack-dev-server': '^3',
+    webpack: '^5',
+    'webpack-cli': '^4',
     'mini-css-extract-plugin': '^0', // Currently < 1
     'clean-webpack-plugin': '^3',
     'html-webpack-plugin': '^4',
-    'glob-all': 'latest', // Always install latest.
+    'glob-all': 'latest', // Always install the latest.
     'file-loader': '^6',
     'css-loader': '^3',
+    'sass': '^1',
     'sass-loader': '^8',
-    'node-sass': '^4',
     'terser-webpack-plugin': '^2',
 
     // BABEL - http://bit.ly/2IdVyKe
@@ -69,7 +68,7 @@ const dependencies = ({ mongo, redux, router, server }) => {
 
     // OTHER
     'cross-env': '^7',
-    'npm-run-all': 'latest', // Always install latest.
+    'npm-run-all': 'latest', // Always install the latest.
     dotenv: !server && '^8' // This is also below in `serverDependencies`.
   }
 
@@ -82,7 +81,7 @@ const dependencies = ({ mongo, redux, router, server }) => {
     helmet: '^3',
     compression: '^1',
     'body-parser': '^1',
-    nodemon: 'latest', // Always install latest.
+    nodemon: 'latest', // Always install the latest.
     dotenv: '^8', // This is also conditionally above in `devDependencies`.
 
     // MONGO

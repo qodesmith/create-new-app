1b96d7523d81e4cd5eace19f8fa1627b6cb02813
Qodesmith <theqodesmith@gmail.com>
2021-01-16 18:30:31 -0500

    Prettier 🙂

--- modules/copySafe.js
+++ modules/copySafe.js
@@ -7,8 +7,7 @@ const fs = require('fs-extra')
 const path = require('path')
 const keepOldFileContent = require('./keepOldFileContent')
 
-
-function copySafe({ sourcePath, destinationPath, excludedFiles = [] }) {
+function copySafe({sourcePath, destinationPath, excludedFiles = []}) {
   const stats = fs.statSync(sourcePath)
   const isDirectory = stats.isDirectory()
 
@@ -29,15 +28,17 @@ function copySafe({ sourcePath, destinationPath, excludedFiles = [] }) {
       })
     })
 
-  // FILES
+    // FILES
   } else {
     // Ignore `.DS_Store` and excluded files.
     if (
       sourcePath.endsWith('.DS_Store') ||
       excludedFiles.some(excludedFile => sourcePath.endsWith(excludedFile))
-    ) return
+    ) {
+      return
+    }
 
-    const newFileContents = keepOldFileContent({ sourcePath, destinationPath })
+    const newFileContents = keepOldFileContent({sourcePath, destinationPath})
     const destinationDir = path.resolve(destinationPath, '..')
 
     fs.ensureDirSync(destinationDir) // In case the directory for the new file doesn't exist, create it.

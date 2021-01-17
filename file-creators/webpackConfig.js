92c20d60ed865705980b66875e7070d5c6b09f69
Qodesmith <theqodesmith@gmail.com>
2021-01-17 08:00:54 -0500

    Processed webpackConfig.js

--- file-creators/webpackConfig.js
+++ file-creators/webpackConfig.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-function webpackConfig({ redux, title, description }) {
+function webpackConfig({title, description}) {
   const aliasPlaceholder = '__PLACEHOLDER_WEBPACK_ALIAS__'
   const titlePlaceholder = '__PLACEHOLDER_TITLE__'
   const descriptionPlaceholder = '__PLACEHOLDER_DESCRIPTION__'
@@ -22,23 +22,17 @@ function webpackConfig({ redux, title, description }) {
   */
   const indent = ' '.repeat(aliasLine.search(/\S/) + 2)
 
-  // Construct the appropriate webpack alias object for redux or not.
-  const aliasReduxObject = [
-    `actions: path.resolve(__dirname, 'src/redux/actions'),`,
-    `middleware: path.resolve(__dirname, 'src/redux/middleware'),`,
-    `reducers: path.resolve(__dirname, 'src/redux/reducers')`,
-  ].map(line => `${indent}${line}`).join('\n')
-
-  // Construct the final alias object, possible including `aliasReduxObject` from above.
+  // Construct the final alias object.
   const aliasObject = [
     '{',
     `${indent}components: path.resolve(__dirname, 'src/components'),`,
     `${indent}assets: path.resolve(__dirname, 'src/assets'),`,
     `${indent}hooks: path.resolve(__dirname, 'src/hooks'),`,
-    `${indent}helpers: path.resolve(__dirname, 'src/helpers')${redux ? ',' : ''}`,
-    redux && aliasReduxObject,
-    `${indent.slice(2)}}` // Closing bracket indented 2 spaces closer.
-  ].filter(Boolean).join('\n')
+    `${indent}helpers: path.resolve(__dirname, 'src/helpers')`,
+    `${indent.slice(2)}}`, // Closing bracket indented 2 spaces closer.
+  ]
+    .filter(Boolean)
+    .join('\n')
 
   return config
     .replace(aliasPlaceholder, aliasObject)

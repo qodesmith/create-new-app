21aad7b4ee4653742a6595336e077feb74897731
Qodesmith <theqodesmith@gmail.com>
2021-02-20 08:14:21 -0500

    Added async option to run.js - plus helpful comments.

--- modules/run.js
+++ modules/run.js
@@ -1,12 +1,29 @@
 // This modules runs commands as if typed into the cli.
 
-const {execSync} = require('child_process')
+const {execSync, execFile} = require('child_process')
 
-module.exports = (command, silent) => {
+module.exports = (command, silent, cb) => {
   /*
     The silent option mute's the commands CLI output except for errors.
     This helps keep the CLI looking clean.
+
+                0              1               2
+    Positions: [process.stdin, process.stdout, process.stderr]
+
+    inherit - Child will use parent's stdios. Pass through the corresponding
+              stdio stream to/from the parent process.
+
+    pipe    - Spawned child will not share this with the parent - "mute".
+
+    http://bit.ly/2Z8xzTF
   */
-  const stdio = silent ? ['pipe', 'pipe', 2] : 'inherit' // http://bit.ly/2Z8xzTF
-  return execSync(command, {stdio, encoding: 'utf8'})
+  //                      stdin   stdout  stderr
+  const stdio = silent ? ['pipe', 'pipe', 'inherit'] : 'inherit'
+
+  if (cb) {
+    // execFile has a slight performance advantage over exec.
+    execFile(command, {stdio: 'inherit', encoding: 'utf8'}, cb)
+  } else {
+    return execSync(command, {stdio, encoding: 'utf8'})
+  }
 }

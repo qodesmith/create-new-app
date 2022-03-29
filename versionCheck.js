b4137992df13d004949a5f8167c5f39d3147b864
Qodesmith <theqodesmith@gmail.com>
2022-03-29 11:25:16 -0400

    Slight update to versionCheck.js

--- versionCheck.js
+++ versionCheck.js
@@ -1,30 +1,30 @@
 #!/usr/bin/env node
 // Possible permissions errors? - http://bit.ly/2Z5PKsZ
 
-let nodeVersion
+let nodeVersion = ''
 
 try {
   //     v10    13     1
-  const [major, minor, _patch] = process.version.split('.')
-  nodeVersion = Number(`${major.slice(1)}.${minor}`)
+  const [major, minor, _patch] = process.versions.node.split('.')
+  nodeVersion = `${major}.${minor}`
 } catch (e) {}
 
-const spaces = ' '.repeat(38 - process.version.toString().length)
+const spaces = ' '.repeat(38 - nodeVersion.length)
 
-if (nodeVersion !== undefined && nodeVersion < 10.13) {
-  const message = `
-    ╭───────────────────────────────────────────────────────────────────╮
-    │                                                                   │
-    │  Create New App requires Node >= 10.13.0 and npm >= 7             │
-    │  You're using Node version ${process.version} ${spaces}│
-    │                                                                   │
-    │  Please upgrade Node                                              │
-    │  The easiest way is to use Node Version Manager:                  │
-    │    https://github.com/creationix/nvm                              │
-    │                                                                   │
-    ╰───────────────────────────────────────────────────────────────────╯
-  `
+if (+nodeVersion < 10.13) {
+  const message = [
+    '╭───────────────────────────────────────────────────────────────────╮',
+    '│                                                                   │',
+    '│  Create New App requires Node >= 10.13.0 and npm >= 7             │',
+    `│  You're using Node version ${process.version} ${spaces}│`,
+    '│                                                                   │',
+    '│  Please upgrade Node                                              │',
+    '│  The easiest way is to use Node Version Manager:                  │',
+    '│    https://github.com/creationix/nvm                              │',
+    '│                                                                   │',
+    '╰───────────────────────────────────────────────────────────────────╯',
+  ].join('\n')
 
-  console.log(message)
+  console.warn(message)
   process.exit(1)
 }

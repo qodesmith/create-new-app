8969a38acead6c9d208f65e94bc8ab2a67c78161
Qodesmith <theqodesmith@gmail.com>
2025-12-15 07:01:20 -0500

    Resetting the project to a blank canvas

--- modules/browserslist.js
+++
@@ -1,7 +0,0 @@
-/*
-  Why is something this simple in its own file?
-  Because it's used multiple times in multiple places: `main.js` and `packageJson.js`.
-
-  http://bit.ly/2Z5pejA - why you should avoid `last 2 versions`.
-*/
-module.exports = ['>0.25%', 'not ie 11', 'not op_mini all']

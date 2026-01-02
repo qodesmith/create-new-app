d8d2ad8e3ead9add752945a74c0372a5325187be
Qodesmith <theqodesmith@gmail.com>
2026-01-02 09:08:09 -0500

    Added client and shared constants files

---
+++ src/projects/fullstack/src/client/constantsClient.ts
@@ -0,0 +1,5 @@
+/**
+ * This key is set in sessionStorage and is used to trigger resetting app state
+ * when no authenticated user is found at the root authenticated route.
+ */
+export const resetAppKey = 'reset-app'
---
+++ src/projects/fullstack/src/shared/constantsShared.ts
@@ -0,0 +1,5 @@
+export const betterAuthBasePath = '/api/app-auth'
+
+export const authRoutePath = '/api/authenticated'
+
+export const userRoles = {admin: 'admin', user: 'user'} as const

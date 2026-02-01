523495b9a611d5a4e105a108c5615a2c612569a7
Qodesmith <theqodesmith@gmail.com>
2026-02-01 15:37:13 -0500

    Added sonner component to fullstack app

---
+++ src/projects/fullstack/src/client/components/ui/sonner.tsx
@@ -0,0 +1,46 @@
+import type {CSSProperties} from 'react'
+import type {ToasterProps} from 'sonner'
+
+import {themeAtom} from '@/client/state/globalState'
+
+import {useAtomValue} from 'jotai'
+import {
+  CircleCheckIcon,
+  InfoIcon,
+  Loader2Icon,
+  OctagonXIcon,
+  TriangleAlertIcon,
+} from 'lucide-react'
+import {useMemo} from 'react'
+import {Toaster as Sonner} from 'sonner'
+
+export const Toaster = ({...props}: ToasterProps) => {
+  const theme = useAtomValue(themeAtom)
+  const style = useMemo(() => {
+    return {
+      '--normal-bg': 'var(--popover)',
+      '--normal-text': 'var(--popover-foreground)',
+      '--normal-border': 'var(--border)',
+      '--border-radius': 'var(--radius)',
+    } as CSSProperties
+  }, [])
+  const icons = useMemo(() => {
+    return {
+      success: <CircleCheckIcon className="size-4" />,
+      info: <InfoIcon className="size-4" />,
+      warning: <TriangleAlertIcon className="size-4" />,
+      error: <OctagonXIcon className="size-4" />,
+      loading: <Loader2Icon className="size-4 animate-spin" />,
+    }
+  }, [])
+
+  return (
+    <Sonner
+      theme={theme}
+      className="toaster group"
+      style={style}
+      icons={icons}
+      {...props}
+    />
+  )
+}

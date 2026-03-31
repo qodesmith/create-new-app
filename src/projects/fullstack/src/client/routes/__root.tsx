9b61ba0323d0fc6a7d49aa950543401f0dd6888b
Qodesmith <theqodesmith@gmail.com>
2026-03-30 20:45:34 -0400

    Typo

--- src/projects/fullstack/src/client/routes/__root.tsx
+++ src/projects/fullstack/src/client/routes/__root.tsx
@@ -15,7 +15,7 @@ const TanStackRouterDevtools =
   process.env.NODE_ENV === 'production'
     ? () => null // Render nothing in production
     : lazy(() =>
-        // Lazy load in developmente
+        // Lazy load in development
         import('@tanstack/react-router-devtools').then(res => ({
           default: res.TanStackRouterDevtools,
           // For Embedded Mode

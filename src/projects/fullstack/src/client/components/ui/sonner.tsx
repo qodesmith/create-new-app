ec3bbc122782db75b076debc8cffdc302460c29c
Qodesmith <theqodesmith@gmail.com>
2026-04-07 13:17:41 -0400

    Removed useMemo from sonner component

--- src/projects/fullstack/src/client/components/ui/sonner.tsx
+++ src/projects/fullstack/src/client/components/ui/sonner.tsx
@@ -11,28 +11,25 @@ import {
   OctagonXIcon,
   TriangleAlertIcon,
 } from 'lucide-react'
-import {useMemo} from 'react'
 import {Toaster as Sonner} from 'sonner'
+
+const style = {
+  '--normal-bg': 'var(--popover)',
+  '--normal-text': 'var(--popover-foreground)',
+  '--normal-border': 'var(--border)',
+  '--border-radius': 'var(--radius)',
+} as CSSProperties
+
+const icons = {
+  success: <CircleCheckIcon className="size-4" />,
+  info: <InfoIcon className="size-4" />,
+  warning: <TriangleAlertIcon className="size-4" />,
+  error: <OctagonXIcon className="size-4" />,
+  loading: <Loader2Icon className="size-4 animate-spin" />,
+}
 
 export const Toaster = ({...props}: ToasterProps) => {
   const theme = useAtomValue(themeSelector)
-  const style = useMemo(() => {
-    return {
-      '--normal-bg': 'var(--popover)',
-      '--normal-text': 'var(--popover-foreground)',
-      '--normal-border': 'var(--border)',
-      '--border-radius': 'var(--radius)',
-    } as CSSProperties
-  }, [])
-  const icons = useMemo(() => {
-    return {
-      success: <CircleCheckIcon className="size-4" />,
-      info: <InfoIcon className="size-4" />,
-      warning: <TriangleAlertIcon className="size-4" />,
-      error: <OctagonXIcon className="size-4" />,
-      loading: <Loader2Icon className="size-4 animate-spin" />,
-    }
-  }, [])
 
   return (
     <Sonner

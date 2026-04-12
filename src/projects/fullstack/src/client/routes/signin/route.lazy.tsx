c4f88fe9aac27d6f939696085eabf1e49cc4f82c
Qodesmith <theqodesmith@gmail.com>
2026-04-12 17:47:40 -0400

    Log client error for failed signin

--- src/projects/fullstack/src/client/routes/signin/route.lazy.tsx
+++ src/projects/fullstack/src/client/routes/signin/route.lazy.tsx
@@ -11,9 +11,13 @@ import {Input} from '@/client/components/ui/input'
 import {MagicCard} from '@/client/components/ui/magic-card'
 import {defaultAuthedPath} from '@/client/constants'
 import {isValidRoute} from '@/client/lib/isValidRoute'
-import {handleFormSubmitInvalid} from '@/client/lib/utils'
+import {handleFormSubmitInvalid, logClientError} from '@/client/lib/utils'
 import {ResetPasswordDialog} from '@/client/routes/signin/-ResetPasswordDialog'
-import {authClientAtom, isSignedInAtom} from '@/client/state/globalState'
+import {
+  apiClientAtom,
+  authClientAtom,
+  isSignedInAtom,
+} from '@/client/state/globalState'
 import {minPasswordLength} from '@/shared/constants'
 
 import {useForm} from '@tanstack/react-form'
@@ -28,6 +32,7 @@ export const Route = createLazyFileRoute('/signin')({
 function SignInPage() {
   const router = useRouter()
   const authClient = useAtomValue(authClientAtom)
+  const apiClient = useAtomValue(apiClientAtom)
   const setIsSignedIn = useSetAtom(isSignedInAtom)
   const {redirect, dialogInitialOpen} = Route.useSearch()
   const redirectPath = isValidRoute(router, redirect)
@@ -54,8 +59,13 @@ function SignInPage() {
 
         setIsSignedIn(true)
         await router.navigate({to: redirectPath})
-      } catch {
+      } catch (error) {
         toast.error('An unexpected error occurred')
+        logClientError({
+          error,
+          context: 'client:signInException',
+          apiClient,
+        })
       }
     },
   })

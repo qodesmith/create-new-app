e025d1dd46701c901b869582c4a98ebb3d4e8fb9
Qodesmith <theqodesmith@gmail.com>
2026-04-12 22:01:06 -0400

    Adding comment about type casting for context

--- src/projects/fullstack/src/server/middleware/adminMiddleware.ts
+++ src/projects/fullstack/src/server/middleware/adminMiddleware.ts
@@ -9,6 +9,7 @@ import {createMiddleware} from 'hono/factory'
 export const adminMiddleware = every(
   authMiddleware, // Sets `user` on the context object.
   createMiddleware(async (c, next) => {
+    // Type casting here because types don't fall through from authMiddleware.
     const user = c.get('user') as SessionData['user'] | undefined
 
     if (!user) {

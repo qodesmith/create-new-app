9e37d3bdb6477647707e200248c41558fddd50ee
Qodesmith <theqodesmith@gmail.com>
2026-04-12 21:47:01 -0400

    Use fly.io header for ipaddress instead of cloudflare; throw when no ipaddress available

--- src/projects/fullstack/src/server/middleware/rateLimitMiddleware.ts
+++ src/projects/fullstack/src/server/middleware/rateLimitMiddleware.ts
@@ -7,12 +7,12 @@ import {bestEffort} from '@/server/utils/bestEffort'
 
 import {getUnitInMs} from '@qodestack/utils'
 import {getConnInfo} from 'hono/bun'
+import {HTTPException} from 'hono/http-exception'
 import {rateLimiter} from 'hono-rate-limiter'
 
 export function getRateLimitMiddleware({
   windowMs = getUnitInMs(1, 'h'), // 1 hour
   limit = 10, // 10 requests per hour per IP
-  logUnknownError = true,
 } = {}): MiddlewareHandler {
   const devMiddleware: MiddlewareHandler = (_c, next) => next()
 
@@ -29,13 +29,12 @@ export function getRateLimitMiddleware({
            * `remote.address` is typically the proxy's IP, not the client's.
            */
           const ipAddress =
-            c.req.header('cf-connecting-ip') ||
+            c.req.header('fly-client-ip') ||
             c.req.header('x-forwarded-for')?.split(',')[0]?.trim() ||
             c.req.header('x-real-ip') ||
-            info.remote.address ||
-            'unknown'
+            info.remote.address
 
-          if (ipAddress === 'unknown' && logUnknownError) {
+          if (!ipAddress) {
             const db = getDatabase()
 
             bestEffort(() => {
@@ -51,6 +50,8 @@ export function getRateLimitMiddleware({
                 })
                 .run()
             })
+
+            throw new HTTPException(403, {message: 'Forbidden'})
           }
 
           return ipAddress

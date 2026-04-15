ff36694b5b6094c5769dc6886fe589b330cf3fcf
Qodesmith <theqodesmith@gmail.com>
2026-04-15 08:06:57 -0400

    Added context comment

--- src/projects/client-only-react/src/lib/isValidRoute.ts
+++ src/projects/client-only-react/src/lib/isValidRoute.ts
@@ -7,6 +7,11 @@ export function isValidRoute(
 ): path is keyof FileRoutesByTo {
   if (!path) return false
 
+  /**
+   * We compute the set on each call instead of caching at the module scope:
+   * 1. In dev, routes may change
+   * 2. This is trivial work
+   */
   const routePathsSet = new Set(
     Object.keys(router.buildRouteTree().routesByPath)
   )

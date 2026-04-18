d23de03efef0107f0703fb8d1195db1b0894292e
Qodesmith <theqodesmith@gmail.com>
2026-04-17 20:51:12 -0400

    Use id for aria-controls in input component

--- src/projects/fullstack/src/client/components/custom/PasswordInput.tsx
+++ src/projects/fullstack/src/client/components/custom/PasswordInput.tsx
@@ -41,12 +41,12 @@ export function PasswordInput({
           {...props}
         />
         <button
-          className="absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-md text-muted-foreground/80 outline-none transition-[color,box-shadow] hover:text-foreground focus:z-10 focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+          className="absolute inset-e-0 inset-y-0 flex h-full w-9 items-center justify-center rounded-e-md text-muted-foreground/80 outline-none transition-[color,box-shadow] hover:text-foreground focus:z-10 focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
           type="button"
           onClick={toggleVisibility}
           aria-label={isVisible ? 'Hide password' : 'Show password'}
           aria-pressed={isVisible}
-          aria-controls="password"
+          aria-controls={id}
         >
           {isVisible ? (
             <EyeOffIcon size={16} aria-hidden="true" />
